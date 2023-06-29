11426798b1fd56f8852a48a4111eb8f982221a3a
GreenCatInForest <event2016.2017@gmail.com>
2023-06-29 10:45:23 +0100

    add image at Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
+import Image from "mui-image";
+import goatFrontView from "../images/goatFrontView.jpg";
+
 import { NavBar } from "../components/NavBar";
 
 export const Home = () => {
   return (
     <div>
       <NavBar />
+      <Image src={goatFrontView} />
     </div>
   );
 };
