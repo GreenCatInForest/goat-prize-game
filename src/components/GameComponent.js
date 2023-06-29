715fd303696d0be94ac91ddb6ec7bbe34ccd272b
GreenCatInForest <event2016.2017@gmail.com>
2023-06-29 18:24:42 +0100

    change handleDoorGenerate

--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -3,40 +3,44 @@ import { useState } from "react";
 import { Button } from "@mui/material";
 
 export const GameComponent = () => {
+  let doors = [
+    {
+      id: 1,
+      name: "door1",
+      doorPrize: "",
+      isOpen: false,
+      choicePlayerDoor: false,
+      choiceMontyDoor: false,
+    },
+
+    {
+      id: 2,
+      name: "door2",
+      doorPrize: "",
+      isOpen: false,
+      choicePlayerDoor: false,
+      choiceMontyDoor: false,
+    },
+
+    {
+      id: 3,
+      name: "door3",
+      doorPrize: "",
+      isOpen: false,
+      choicePlayerDoor: false,
+      choiceMontyDoor: false,
+    },
+  ];
   const handleDoorGenerator = () => {
-    let doors = [
-      {
-        id: 1,
-        name: "door1",
-        doorPrize: "",
-        isOpen: false,
-        choicePlayerDoor: false,
-        choiceMontyDoor: false,
-      },
-
-      {
-        id: 2,
-        name: "door2",
-        doorPrize: "",
-        isOpen: false,
-        choicePlayerDoor: false,
-        choiceMontyDoor: false,
-      },
-
-      {
-        id: 3,
-        name: "door3",
-        doorPrize: "",
-        isOpen: false,
-        choicePlayerDoor: false,
-        choiceMontyDoor: false,
-      },
-    ];
     let count = Math.floor(Math.random() * 3 + 1);
 
     doors.map((door) =>
       door.id === count ? (door.doorPrize = "prize") : (door.doorPrize = "goat")
     );
+
+    if (doors.doorPrize === "prize") {
+      return;
+    }
 
     console.log(doors);
   };
