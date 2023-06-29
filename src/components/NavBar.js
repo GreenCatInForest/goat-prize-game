bcc6ee73ee15aba3df8477d7deb5afafeb7e68fa
GreenCatInForest <event2016.2017@gmail.com>
2023-06-29 08:45:17 +0100

    add mobile responsive design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,100 +13,49 @@ import Settings from "@mui/icons-material/Settings";
 import Logout from "@mui/icons-material/Logout";
 
 export const NavBar = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <React.Fragment>
       <Box
         sx={{
-          display: "flex",
+          display: {
+            xs: "flex",
+            sm: "none",
+          },
           flexDirection: "column",
           alignItems: "center",
           textAlign: "center",
         }}
       >
-        <Typography sx={{ minWidth: 100 }}>Contact</Typography>
-        <Typography sx={{ minWidth: 100 }}>Profile</Typography>
-        <Tooltip title="Account settings">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
+        <Tooltip title="Home Page">
+          <IconButton size="small">
             <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
           </IconButton>
         </Tooltip>
+        <Typography sx={{ minWidth: 100 }}>About</Typography>
+        <Typography sx={{ minWidth: 100 }}>FAQ</Typography>
+        <Typography sx={{ minWidth: 100 }}>Contacts</Typography>
       </Box>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            "&:before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
+      <Box
+        sx={{
+          display: {
+            xs: "none",
+            sm: "flex",
           },
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          textAlign: "center",
         }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <MenuItem onClick={handleClose}>
-          <Avatar /> Profile
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
+        <Tooltip title="Home Page">
+          <IconButton size="small">
+            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+          </IconButton>
+        </Tooltip>
+        <Typography sx={{ minWidth: 100 }}>About</Typography>
+        <Typography sx={{ minWidth: 100 }}>FAQ</Typography>
+        <Typography sx={{ minWidth: 100 }}>Contacts</Typography>
+      </Box>
     </React.Fragment>
   );
 };
